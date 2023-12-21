056d033c064378480dd95517721ef898b10dcce3
Anthony Clark <a0c01a7@HOMEOFFICE.wal-mart.com>
2023-12-21 15:26:05 -0600

    changes

--- Auto/MainForm.Designer.cs
+++ Auto/MainForm.Designer.cs
@@ -33,18 +33,17 @@ namespace Auto
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel_bottom = new System.Windows.Forms.Panel();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.button_Submit = new System.Windows.Forms.Button();
             this.button_Generate = new System.Windows.Forms.Button();
             this.button_Clear = new System.Windows.Forms.Button();
             this.panel_left = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Issue = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox_location = new System.Windows.Forms.ComboBox();
             this.panel_right = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox_userID = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox_Description = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -65,7 +64,6 @@ namespace Auto
             // 
             // panel_bottom
             // 
-            this.panel_bottom.Controls.Add(this.statusStrip1);
             this.panel_bottom.Controls.Add(this.button_Submit);
             this.panel_bottom.Controls.Add(this.button_Generate);
             this.panel_bottom.Controls.Add(this.button_Clear);
@@ -75,18 +73,9 @@ namespace Auto
             this.panel_bottom.Size = new System.Drawing.Size(1174, 159);
             this.panel_bottom.TabIndex = 0;
             // 
-            // statusStrip1
-            // 
-            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.statusStrip1.Location = new System.Drawing.Point(0, 137);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1174, 22);
-            this.statusStrip1.TabIndex = 3;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
             // button_Submit
             // 
-            this.button_Submit.Location = new System.Drawing.Point(730, 41);
+            this.button_Submit.Location = new System.Drawing.Point(720, 41);
             this.button_Submit.Name = "button_Submit";
             this.button_Submit.Size = new System.Drawing.Size(239, 77);
             this.button_Submit.TabIndex = 2;
@@ -95,7 +84,7 @@ namespace Auto
             // 
             // button_Generate
             // 
-            this.button_Generate.Location = new System.Drawing.Point(468, 41);
+            this.button_Generate.Location = new System.Drawing.Point(458, 41);
             this.button_Generate.Name = "button_Generate";
             this.button_Generate.Size = new System.Drawing.Size(239, 77);
             this.button_Generate.TabIndex = 1;
@@ -104,7 +93,7 @@ namespace Auto
             // 
             // button_Clear
             // 
-            this.button_Clear.Location = new System.Drawing.Point(202, 41);
+            this.button_Clear.Location = new System.Drawing.Point(192, 41);
             this.button_Clear.Name = "button_Clear";
             this.button_Clear.Size = new System.Drawing.Size(239, 77);
             this.button_Clear.TabIndex = 0;
@@ -135,7 +124,7 @@ namespace Auto
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Name,
+            this.NameColumn,
             this.Issue});
             dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
@@ -145,7 +134,7 @@ namespace Auto
             dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Location = new System.Drawing.Point(13, 78);
             this.dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
@@ -160,14 +149,14 @@ namespace Auto
             this.dataGridView1.Size = new System.Drawing.Size(634, 626);
             this.dataGridView1.TabIndex = 2;
             // 
-            // Name
+            // NameColumn
             // 
-            this.Name.Frozen = true;
-            this.Name.HeaderText = "Name";
-            this.Name.MinimumWidth = 10;
-            this.Name.Name = "Name";
-            this.Name.ReadOnly = true;
-            this.Name.Width = 200;
+            this.NameColumn.Frozen = true;
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.MinimumWidth = 10;
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.ReadOnly = true;
+            this.NameColumn.Width = 200;
             // 
             // Issue
             // 
@@ -180,27 +169,39 @@ namespace Auto
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(528, 4);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(528, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 64);
+            this.button1.Size = new System.Drawing.Size(118, 56);
             this.button1.TabIndex = 1;
             this.button1.Text = "Fetch";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // comboBox_location
             // 
-            this.comboBox_location.BackColor = System.Drawing.Color.AliceBlue;
+            this.comboBox_location.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox_location.BackColor = System.Drawing.SystemColors.Control;
             this.comboBox_location.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox_location.DropDownHeight = 110;
+            this.comboBox_location.Font = new System.Drawing.Font("Bogle", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_location.FormattingEnabled = true;
-            this.comboBox_location.ItemHeight = 25;
-            this.comboBox_location.Location = new System.Drawing.Point(353, 12);
+            this.comboBox_location.IntegralHeight = false;
+            this.comboBox_location.ItemHeight = 38;
+            this.comboBox_location.Items.AddRange(new object[] {
+            "HomeOffice",
+            "DGTC",
+            "J Street",
+            "Sam\'s Club",
+            "Mobile"});
+            this.comboBox_location.Location = new System.Drawing.Point(343, 17);
             this.comboBox_location.Name = "comboBox_location";
-            this.comboBox_location.Size = new System.Drawing.Size(169, 33);
+            this.comboBox_location.Size = new System.Drawing.Size(179, 46);
             this.comboBox_location.TabIndex = 0;
+            this.comboBox_location.Text = "Location";
             // 
             // panel_right
             // 
-            this.panel_right.Controls.Add(this.textBox2);
+            this.panel_right.Controls.Add(this.textBox_userID);
             this.panel_right.Controls.Add(this.textBox1);
             this.panel_right.Controls.Add(this.textBox_Description);
             this.panel_right.Controls.Add(this.label3);
@@ -219,35 +220,35 @@ namespace Auto
             this.panel_right.Size = new System.Drawing.Size(518, 770);
             this.panel_right.TabIndex = 2;
             // 
-            // textBox2
+            // textBox_userID
             // 
-            this.textBox2.Location = new System.Drawing.Point(29, 52);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 46);
-            this.textBox2.TabIndex = 15;
+            this.textBox_userID.Font = new System.Drawing.Font("Bogle", 7.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_userID.Location = new System.Drawing.Point(94, 30);
+            this.textBox_userID.Name = "textBox_userID";
+            this.textBox_userID.Size = new System.Drawing.Size(169, 32);
+            this.textBox_userID.TabIndex = 15;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 664);
+            this.textBox1.Location = new System.Drawing.Point(29, 648);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(477, 84);
+            this.textBox1.Size = new System.Drawing.Size(477, 100);
             this.textBox1.TabIndex = 14;
             // 
             // textBox_Description
             // 
-            this.textBox_Description.Location = new System.Drawing.Point(29, 542);
+            this.textBox_Description.Location = new System.Drawing.Point(29, 515);
             this.textBox_Description.Multiline = true;
             this.textBox_Description.Name = "textBox_Description";
-            this.textBox_Description.Size = new System.Drawing.Size(477, 71);
+            this.textBox_Description.Size = new System.Drawing.Size(477, 98);
             this.textBox_Description.TabIndex = 13;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bogle", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 628);
+            this.label3.Location = new System.Drawing.Point(13, 618);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(137, 33);
             this.label3.TabIndex = 12;
@@ -257,7 +258,7 @@ namespace Auto
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bogle", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 506);
+            this.label2.Location = new System.Drawing.Point(13, 484);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(137, 33);
             this.label2.TabIndex = 11;
@@ -267,19 +268,22 @@ namespace Auto
             // 
             this.comboBox_Subject.BackColor = System.Drawing.Color.AliceBlue;
             this.comboBox_Subject.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox_Subject.Font = new System.Drawing.Font("Bogle", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_Subject.FormattingEnabled = true;
-            this.comboBox_Subject.ItemHeight = 25;
-            this.comboBox_Subject.Location = new System.Drawing.Point(337, 360);
+            this.comboBox_Subject.ItemHeight = 38;
+            this.comboBox_Subject.Location = new System.Drawing.Point(182, 360);
             this.comboBox_Subject.Name = "comboBox_Subject";
-            this.comboBox_Subject.Size = new System.Drawing.Size(169, 33);
+            this.comboBox_Subject.Size = new System.Drawing.Size(324, 46);
             this.comboBox_Subject.TabIndex = 10;
+            this.comboBox_Subject.SelectedIndexChanged += new System.EventHandler(this.comboBox_Subject_SelectedIndexChanged);
             // 
             // radioButton_Mobile
             // 
             this.radioButton_Mobile.AutoSize = true;
-            this.radioButton_Mobile.Location = new System.Drawing.Point(390, 52);
+            this.radioButton_Mobile.Font = new System.Drawing.Font("Bogle", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_Mobile.Location = new System.Drawing.Point(166, 78);
             this.radioButton_Mobile.Name = "radioButton_Mobile";
-            this.radioButton_Mobile.Size = new System.Drawing.Size(107, 29);
+            this.radioButton_Mobile.Size = new System.Drawing.Size(106, 33);
             this.radioButton_Mobile.TabIndex = 8;
             this.radioButton_Mobile.TabStop = true;
             this.radioButton_Mobile.Text = "Mobile";
@@ -288,9 +292,10 @@ namespace Auto
             // radioButton_MAC
             // 
             this.radioButton_MAC.AutoSize = true;
-            this.radioButton_MAC.Location = new System.Drawing.Point(290, 52);
+            this.radioButton_MAC.Font = new System.Drawing.Font("Bogle", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_MAC.Location = new System.Drawing.Point(78, 78);
             this.radioButton_MAC.Name = "radioButton_MAC";
-            this.radioButton_MAC.Size = new System.Drawing.Size(90, 29);
+            this.radioButton_MAC.Size = new System.Drawing.Size(91, 33);
             this.radioButton_MAC.TabIndex = 7;
             this.radioButton_MAC.TabStop = true;
             this.radioButton_MAC.Text = "MAC";
@@ -299,9 +304,10 @@ namespace Auto
             // radioButton_PC
             // 
             this.radioButton_PC.AutoSize = true;
-            this.radioButton_PC.Location = new System.Drawing.Point(212, 52);
+            this.radioButton_PC.Font = new System.Drawing.Font("Bogle", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_PC.Location = new System.Drawing.Point(12, 78);
             this.radioButton_PC.Name = "radioButton_PC";
-            this.radioButton_PC.Size = new System.Drawing.Size(72, 29);
+            this.radioButton_PC.Size = new System.Drawing.Size(71, 33);
             this.radioButton_PC.TabIndex = 6;
             this.radioButton_PC.TabStop = true;
             this.radioButton_PC.Text = "PC";
@@ -309,6 +315,7 @@ namespace Auto
             // 
             // button_Issue
             // 
+            this.button_Issue.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button_Issue.Location = new System.Drawing.Point(349, 273);
             this.button_Issue.Name = "button_Issue";
             this.button_Issue.Size = new System.Drawing.Size(160, 71);
@@ -319,6 +326,7 @@ namespace Auto
             // 
             // button_Equipment
             // 
+            this.button_Equipment.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button_Equipment.Location = new System.Drawing.Point(182, 273);
             this.button_Equipment.Name = "button_Equipment";
             this.button_Equipment.Size = new System.Drawing.Size(160, 71);
@@ -329,6 +337,7 @@ namespace Auto
             // 
             // button_Question
             // 
+            this.button_Question.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button_Question.Location = new System.Drawing.Point(16, 273);
             this.button_Question.Name = "button_Question";
             this.button_Question.Size = new System.Drawing.Size(160, 71);
@@ -340,10 +349,10 @@ namespace Auto
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bogle", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Font = new System.Drawing.Font("Bogle", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 38);
+            this.label1.Size = new System.Drawing.Size(87, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "UserID";
             // 
@@ -358,12 +367,11 @@ namespace Auto
             this.Controls.Add(this.panel_bottom);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            //this.Name = "MainForm";
+            this.Name = "MainForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auto IMS";
             this.panel_bottom.ResumeLayout(false);
-            this.panel_bottom.PerformLayout();
             this.panel_left.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel_right.ResumeLayout(false);
@@ -377,8 +385,6 @@ namespace Auto
         private System.Windows.Forms.Panel panel_bottom;
         private System.Windows.Forms.Panel panel_left;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Issue;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox_location;
         private System.Windows.Forms.Panel panel_right;
@@ -389,7 +395,6 @@ namespace Auto
         private System.Windows.Forms.Button button_Equipment;
         private System.Windows.Forms.Button button_Question;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.RadioButton radioButton_Mobile;
         private System.Windows.Forms.RadioButton radioButton_MAC;
         private System.Windows.Forms.RadioButton radioButton_PC;
@@ -397,8 +402,10 @@ namespace Auto
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox_Description;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox_userID;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Issue;
     }
 }
 
--- Auto/MainForm.cs
+++ Auto/MainForm.cs
@@ -10,9 +10,14 @@ using System.Windows.Forms;
 
 namespace Auto
 {
+    public enum buttonselection { question, equipment, issue, none};
+
     public partial class MainForm : Form
     {
-        private Button[] allButtons;
+        public static Button[] allButtons;
+        public static buttonselection buttselect;
+        private static string selectedbuttonname;
+
 
         public MainForm()
         {
@@ -22,36 +27,58 @@ namespace Auto
 
         private void InitializeBase()
         {
-            // Initialize the array with all your buttons
-            allButtons = new Button[] { button_Equipment, button_Question, button_Issue };
             ComboBoxUpdater.CreateDictionaries();
+            allButtons = new Button[] { button_Equipment, button_Question, button_Issue };      
+            buttselect = buttonselection.none;
         }
 
         private void button_Equipment_Click(object sender, EventArgs e)
         {
-            ComboBoxUpdater.PopulateDropDown(comboBox_Subject, "equipment");
-
-            // Update the appearance of the clicked button and reset others
-            Functions.UpdateButtonAppearance(button_Equipment);
-            Functions.ResetAllButtonsAppearance(button_Question, button_Issue);
+            buttoncontroller(button_Equipment);
         }
 
         private void button_Question_Click(object sender, EventArgs e)
         {
-            ComboBoxUpdater.PopulateDropDown(comboBox_Subject, "question");
-
-            // Update the appearance of the clicked button and reset others
-            Functions.UpdateButtonAppearance(button_Question);
-            Functions.ResetAllButtonsAppearance(button_Question, button_Issue);
+            buttoncontroller(button_Question);
         }
 
         private void button_Issue_Click(object sender, EventArgs e)
         {
-            ComboBoxUpdater.PopulateDropDown(comboBox_Subject, "issue");
+            buttoncontroller(button_Issue);
+        }
 
-            // Update the appearance of the clicked button and reset others
-            Functions.UpdateButtonAppearance(button_Issue);
-            Functions.ResetAllButtonsAppearance(button_Question, button_Issue);
+        private void buttoncontroller(Button but)
+        {
+            buttselect = enumcontroller(but.Name.ToLower());
+            Functions.HandleButtonSelection(but, allButtons, comboBox_Subject);
+        }
+
+        private buttonselection enumcontroller(string butname)
+        {
+            buttonselection butsel = new buttonselection();
+
+            switch (butname)
+            {
+                case "button_Question":
+                    butsel = buttonselection.question;
+                    break;
+                case "button_Equipment":
+                    butsel = buttonselection.equipment;
+                    break;
+                case "button_Issue":
+                    butsel = buttonselection.issue;
+                    break;
+                case "none":
+                    butsel = buttonselection.none;
+                    break;
+            }
+
+            return butsel;
+        }
+
+        private void comboBox_Subject_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
